916428ea3c5fa23b2389e2db1bf910fe7a3a0c4a
Elalitareq <elalitareq@gmail.com>
2023-05-30 00:18:10 +0300

    fixed another new thing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import GoogleProvider from "next-auth/providers/google";
 // import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
