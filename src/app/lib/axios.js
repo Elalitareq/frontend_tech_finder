2bf3a5bf035fcae33ae037a7678abc51a9a6b23f
Elalitareq <117711772+Elalitareq@users.noreply.github.com>
2023-06-06 09:30:23 +0300

    Update axios.js

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -2,8 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://techfinder.onrender.com/api",
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://techfinder.onrender.com/api",
+  // baseURL: "http://localhost:4000/api",
   "content-type": "application/json",
 });
 export { api };
